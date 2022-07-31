290fe0ebe29413d2b34a97d7d3249bc5c43d8684
jellewestenberger <jellewestenberger@gmail.com>
2022-07-31 11:39:25 +0200

    moved influxdb parser to main file, updated readme

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# PolarSteps-JSON2gpx-Parser
-This script parses the GPS file from your exported Polarsteps data to a gpx 1.0 scheme.
+# PolarSteps/InfluxDB to gpx Parser
+This script parses the GPS file from your exported Polarsteps or InfluxDB data to a gpx 1.0 scheme.
 
 This could be a helpful tool in many different scenarios, but I created it to be able to use the gps trackpoints from Polarsteps to automatically geotag my photos in Adobe Lightroom (which only accepts gpx tracklogs). <a href="https://helpx.adobe.com/lightroom/help/maps-module.html#timezone_offset_auto_tag" target="_blank">Read here how to use tracklogs to auto-tag your photos in Lightroom</a>
 Note that the script assumes that the unix time stamps are in the GMT+0 time zone, therefore you may need to compensate for the time set in your camera. Lightroom has the option to do this.
 
 The script may not contain the most elegant or robust code, but it was very useful to me, and may be useful to you too. 
 
+# InfluxDB
+Since I also log my phone's coordinates to InfluxDB (via HomeAssistant), I also added the functionality to convert this gps data to the gpx scheme. 
 
+It assumes that the csv files are influxdb exports and that the gpx data is formatted like:
+
+    "time","state.mean_latitude","state.mean_longitude"
+    "2022-07-19T17:29:00.000+02:00","54.2820671","9.7710714"
+    "2022-07-19T17:30:00.000+02:00","54.2963866","9.759142266666666"
+    "2022-07-19T17:31:00.000+02:00","54.3107061","9.747213133333334"
+The header is skipped, so the specific format doesn't matter there.
--- main.py
+++ main.py
@@ -1,76 +1,102 @@
 import os 
 import datetime
 import numpy as np
+import glob
+import subprocess
+
+# converting json files (polarsteps export)
+def convertjson(gpsdat,name):
+    dat=open(gpsdat)
+    waypoints=dat.readlines()
+    dat.close()
+
+    waypoints=waypoints[0].split('[')
+    waypoints=waypoints[1].split('{')
+
+    lon=[]
+    lat=[]
+    timel=[]
+
+    print("Parsing..") 
+    for i in range(len(waypoints)):
+        if i%100==0:
+            print("%f " %((100.*i/len(waypoints))))
+        line=waypoints[i]
+        line=line.replace(' ', '')
+        line=line.replace('}','')
+        line=line.replace(']','')
+        if len(line)>0:
+            line=line.split(',')
+            
+            for j in range(len(line)):
+                seg=line[j].split(':')
+                if seg[0]=='"lon"':
+                    lon.append(float(seg[1]))
+                elif seg[0]=='"lat"':
+                    lat.append(float(seg[1]))
+                elif seg[0]=='"time"':
+                    timel.append(float(seg[1]))
+
+    sortindex = np.argsort(timel)
+    timel=[timel[i] for i in sortindex]
+    lon=[lon[i] for i in sortindex]
+    lat=[lat[i] for i in sortindex]
+
+    lines=['<?xml version="1.0" encoding="UTF-8" ?><gpx version="1.0" creator="%s" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/0" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">\n \n <trk><trkseg>\n'%name]
+
+    for i in range(len(timel)):
+        date=datetime.datetime.fromtimestamp(timel[i])
+    
+
+    
+        ds=date.isoformat()+"Z" # Indicates UTC time
+        s='<trkpt lat="%f" lon="%f"><time>%s</time><src>polarsteps</src></trkpt>\n' %(lat[i],lon[i],ds)
+        lines.append(s)
+    lines.append("</trkseg></trk></gpx>")
+    exp=open(gpsdat[:-5]+"_converted.gpx", "w+")
+    exp.writelines(lines)
+    exp.close()
+    print("Done,\n exported to %s"%(gpsdat[:-5]+"_converted.gpx"))
+
+# converting csv files (influxdb export)
+def convertcsv(file):
+    source = "influxdb"
+    outputfile = open(file.replace(".csv","_converted.gpx"),'w')
+
+    data =np.genfromtxt(file,delimiter=",",skip_header=1,dtype='str')
+
+    lines = ['<?xml version="1.0" encoding="UTF-8" ?><gpx version="1.0" creator="Jelle Westenberger" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/0" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">\n','<trk><trkseg>\n\n']
+    for i in range(len(data)):
+        line = data[i]
+
+        if line[1]!="":
+            time = line[0].replace('"','')
+
+            lat = line[1].replace('"','')
+            lon = line[2].replace('"','')
+            lines.append('<trkpt lat="%s" lon="%s"><time>%s</time><src>%s</src></trkpt>\n' % (lat,lon,time,source))
+    lines.append("</trkseg></trk></gpx>")
+
+    outputfile.writelines(lines)
+    outputfile.close()
+    print("Done,\n exported to %s"%outputfile.name)
 
 
-path=os.path.abspath(__file__)
-path=path.replace("\\","/")
-fname=path.split("/")[-1]
-path=path.replace(fname,"")
-files=os.listdir(path)
-j=0
-print("Located json files: ")
-while j<len(files):
-    if files[j][-4:].lower()=='json':
-        print("%s (%d)" %(files[j],j))
-        j+=1
-    else:
-        del files[j]
-selfile=files[int(input("select json file "))]
-gpsdat=path+selfile
-dat=open(gpsdat)
-waypoints=dat.readlines()
-dat.close()
+jsonfiles = glob.glob("./*.json")
+print("found %i json files:" % (len(jsonfiles)))
 
-waypoints=waypoints[0].split('[')
-waypoints=waypoints[1].split('{')
-
-lon=[]
-lat=[]
-timel=[]
-
-print("Parsing..") 
-for i in range(len(waypoints)):
-    if i%100==0:
-        print("%f " %((100.*i/len(waypoints))))
-    line=waypoints[i]
-    line=line.replace(' ', '')
-    line=line.replace('}','')
-    line=line.replace(']','')
-    if len(line)>0:
-        line=line.split(',')
-        
-        for j in range(len(line)):
-            seg=line[j].split(':')
-            if seg[0]=='"lon"':
-                lon.append(float(seg[1]))
-            elif seg[0]=='"lat"':
-                lat.append(float(seg[1]))
-            elif seg[0]=='"time"':
-                timel.append(float(seg[1]))
-
-sortindex = np.argsort(timel)
-timel=[timel[i] for i in sortindex]
-lon=[lon[i] for i in sortindex]
-lat=[lat[i] for i in sortindex]
-
-lines=['<?xml version="1.0" encoding="UTF-8" ?><gpx version="1.0" creator="Jelle Westenberger" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/0" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">\n \n <trk><trkseg>\n'] 
-
-for i in range(len(timel)):
-    date=datetime.datetime.fromtimestamp(timel[i])
- 
-
- 
-    ds=date.isoformat()+"Z"
-    s='<trkpt lat="%f" lon="%f"><time>%s</time><src>polarsteps</src></trkpt>\n' %(lat[i],lon[i],ds)
-    lines.append(s)
-    dummy=2
-lines.append("</trkseg></trk></gpx>")
-exp=open(path+selfile[:-5]+"_converted.gpx", "w+")
-exp.writelines(lines)
-exp.close()
-print("Done,\n exported to %s"%(selfile[:-5]+"_converted.gpx"))
 try:
-    input('Finished')
+    name = subprocess.check_output("git config user.name", shell=True).decode("utf-8").replace('\n','')
+    print("using git name: %s" % (name))
 except:
-    dummy=2
+    name = str(input("Input your name: "))
+
+for f in jsonfiles:
+    print("- %s" % (f))
+    convertjson(f,name)
+
+csvfiles = glob.glob("./*.csv")
+print("found %i csv files:" % (len(csvfiles)))
+for f in csvfiles:
+    print("- %s" % (f))
+    convertcsv(f)
